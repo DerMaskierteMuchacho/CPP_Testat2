6f1d026f25d5c7ecca93c3c34bdfc33c8aeb7720
djroumen <64631221+djroumen@users.noreply.github.com>
2020-11-03 12:05:16 +0100

    changes for ALF

--- CPP_Testat2/testatLib/kwic.cpp
+++ CPP_Testat2/testatLib/kwic.cpp
@@ -1,9 +1,11 @@
-#include "kwic.hpp"
-#include "word.hpp"
-//#include <istream>
-#include <iostream>
+#include "kwic.h"
+#include "word.h"
+#include <istream>
+#include <ostream>
+#include <sstream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 namespace text {
 	using linesType = std::vector<Word>;
 	std::vector<linesType> readInput(std::istream& in, std::ostream& out) {
@@ -11,10 +13,10 @@ namespace text {
 		std::vector<Word> wordsFromOneLine{};
 		std::vector<linesType> allLines{};
 		while (std::getline(in, oneLine)) {
-			std::istringstream s(oneLine);
+			std::istringstream s{ oneLine };
 			wordsFromOneLine.insert(wordsFromOneLine.end(),
-				std::istream_iterator<std::string>(s),
-				std::istream_iterator<std::string>());
+				std::istream_iterator<Word>(s),
+				std::istream_iterator<Word>());
 			allLines.push_back(wordsFromOneLine);
 			wordsFromOneLine.clear();
 		}
@@ -25,7 +27,6 @@ namespace text {
 		std::vector<std::vector<Word>> mutations{};
 		std::for_each(allLines.begin(), allLines.end(), [&mutations](linesType& linesType) {
 			for (unsigned int i = 0; i < linesType.size(); i++) {
-				//std::rotate(lines.begin(), lines.begin() + 1, lines.end());
 				linesType.push_back(linesType[0]);
 				linesType.erase(linesType.begin());
 				mutations.push_back(linesType);
@@ -36,8 +37,8 @@ namespace text {
 
 	void printMutations(std::vector<linesType>& mutations, std::ostream& out) {
 		std::for_each(mutations.begin(), mutations.end(), [&out](const linesType& linesType) {
-			std::for_each(linesType.begin(), linesType.end(), [&out](const std::string& str) {
-				out << str << " ";
+			std::for_each(linesType.begin(), linesType.end(), [&out](const Word& word) {
+				out << word << " ";
 				});
 			out << "\n";
 			});
---
+++ CPP_Testat2/testatLib/kwic.h
@@ -0,0 +1,11 @@
+#ifndef KWIC_H_
+#define KWIC_H_
+
+#include <string>
+#include <iosfwd>
+namespace text {
+	void kwic(std::istream &, std::ostream &);
+}
+
+
+#endif /* KWIC_H_ */
